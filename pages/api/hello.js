f263843f407d9a2f12c74a7bebba4f960fb4b746
y0092888 <ju-horn@web.de>
2020-10-29 18:13:37 +0100

    init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  console.log(req.query);
+
+  res.statusCode = 200;
+  res.json({ name: "John Doe" });
+};
